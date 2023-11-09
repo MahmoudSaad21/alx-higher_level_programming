e9a300bf65d9867aec5da6162c2003cad884b16f
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-11-09 03:41:04 +0200

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,6 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
     return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
-
 # mul - returns x*y.
 def mul(x, y):
     return x * y
