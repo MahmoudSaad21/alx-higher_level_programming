7d6433d9e4a73273aa4b4c8aeee5ddbb75fbbc05
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-11-09 02:13:35 +0200

    Sandbox is not working

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list=[]
     for i in my_list:
-        if i==search:
+        if i == search:
             new_list.append(replace)
         else:
             new_list.append(i)
-    return new_list
+    return (new_list)
