50a78cc4e2cd52706079dac92aaae7ca4c873fcb
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-11-09 03:06:10 +0200

    commit 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+  sum = 0
+  for i in roman_string:
+    if i == 'I':
+      sum = sum + 1
+    elif i == 'V':
+      sum = sum + 5
+    elif i == 'x':
+      sum = sum + 10
+    elif i == 'L':
+      sum = sum + 50
+    elif i == 'C':
+      sum = sum + 100
+    elif i == 'D':
+      sum = sum + 500
+    elif i == 'M':
+      sum = sum + 1000
+  return sum
