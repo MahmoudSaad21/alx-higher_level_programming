b571e861b90aec1b37999f80a5512dc89472f0c7
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-11-09 03:21:26 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-  sum = 0
-  for i in roman_string:
-    if i == 'I':
-      sum = sum + 1
-    elif i == 'V':
-      sum = sum + 5
-    elif i == 'X':
-      sum = sum + 10
-    elif i == 'L':
-      sum = sum + 50
-    elif i == 'C':
-      sum = sum + 100
-    elif i == 'D':
-      sum = sum + 500
-    elif i == 'M':
-      sum = sum + 1000
-  return sum
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = roman_numerals.get(char, 0)
+        if value >= prev_value:
+            result += value
+        else:
+            result -= value
+        prev_value = value
+    return result
