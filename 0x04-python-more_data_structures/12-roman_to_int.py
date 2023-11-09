63eb3c4369c1d4a47a66c82a0a331dec629ce9f2
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2023-11-09 03:27:21 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+# roman_to_int - returns a new dictionary with all values multiplied by two.
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     prev_value = 0
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    
     for char in reversed(roman_string):
         val = roman_numerals.get(char, 0)
         if val >= prev_value:
