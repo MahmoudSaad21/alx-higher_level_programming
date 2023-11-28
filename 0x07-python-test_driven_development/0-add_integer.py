fefd7a7bfe3bc2d8c0d9dfcdc541ef0d74fd7583
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-11-28 23:57:45 +0000

    ,

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ Module add-integer Adds two integer together
 """
 
 
-def add_integer(a, b):
+def add_integer(a, b=98):
     """Returns  the addition of a and b"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
