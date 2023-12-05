2b9936d9c21c0bffd46b13203e1a8cfc8647d74a
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-12-05 20:17:09 +0000

    ..

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+    0-read_file: read_file()
+"""
+
+
+
 def read_file(filename=""):
+    """read_file reads text file and prints it"""
     with open(filename, 'r') as files:
         for line in files:
             print(line, end="")
