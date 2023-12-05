d4120deca06b4bb25d62f433aeec72b490ed0f9c
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-12-05 21:17:06 +0000

    ..

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student"""
+        if type(attrs) == list:
+            if type(ele) == str for ele in attrs:
+                return {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+        return self.__dict__
