fad71c8d0ad4e259f21bb70824ce18ca37c49e2f
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2023-12-05 21:43:08 +0000

    ,

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+
 def pascal_triangle(n):
-    """
-    returns a list of lists of integers
-        Args:
-            n (int): number of lists and digits
-        Returns: list of lists
-    """
+    """returns a list of lists of integers"""
     rows = [[1 for j in range(i + 1)] for i in range(n)]
     for j in range(n):
         for i in range(j - 1):
