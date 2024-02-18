d444d88d303286bd7fbd51aaa495ecc9f41d5881
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-02-18 04:00:20 -0800

    ..

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa"""
+"""prints all City objects
+from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
 
     import sys
     from model_state import Base, State
+    from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
